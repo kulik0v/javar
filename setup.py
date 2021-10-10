dab128dc39a4adc99286f7d4272c5598a04c4639
kulikov <kulikov@gmail.com>
2021-10-11 01:10:53 +0300

    better description

--- setup.py
+++ setup.py
@@ -2,8 +2,10 @@ from setuptools import setup
 
 setup(
     name='javar',
-    version='1.0.0',
-    description='',
+    version='0.0.2',
+    description='Javar allows you to generate java cli with a set of options easily.',
+    long_description='Run java binary from python',
+    long_description_content_type='text/plain',
     url='https://github.com/kulik0v/javar',
     # download_url='https://github.com/kulik0v/javar/archive/master.zip',
     author='Alexander Kulikov',
