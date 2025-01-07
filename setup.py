4aa23c3e63f495af8f8b96e2e917093deadd5bf8
kulikov <kulikov@gmail.com>
2025-01-07 02:39:32 +0200

    update home url

--- setup.py
+++ setup.py
@@ -2,16 +2,16 @@ from setuptools import setup
 
 setup(
     name='javar',
-    version='0.0.4',
+    version='0.0.5',
     description='Javar allows you to generate java cli with a set of options easily.',
     long_description='Run java binary from python',
     long_description_content_type='text/plain',
-    url='https://github.com/kulik0v/javar',
-    # download_url='https://github.com/kulik0v/javar/archive/master.zip',
+    url='https://github.com/nagolos/javar',
+    # download_url='https://github.com/nagolos/javar/archive/master.zip',
     author='Alexander Kulikov',
     license='MIT',
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
     ],
